e02014d823b9b6eea6de073f785f8c01d420d9fa
juanfrsnow <127858173+juanfrsnow@users.noreply.github.com>
2023-03-23 14:37:14 +0100

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
  
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
-    with my_cnx.cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
     
@@ -57,7 +57,7 @@ if streamlit.button('Get Fruit Load List'):
 
 streamlit.stop()
     
-#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-#streamlit.write('Thanks for adding ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
-#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
